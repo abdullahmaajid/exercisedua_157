20af1abd8a1e18fc1576263d74eb127db0ffd390
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2023-06-05 14:18:53 +0700

    pragma once

---
+++ agregasi/buku.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef BUKU_H
+#define BUKU_H
+
+class buku {
+public:
+	string judul_buku;
+	vector <pengarang*> daftar_pengarang;
+
+	buku(string pJudul) :judul_buku(pJudul) {
+
+	}
+	void tambahPengarang(pengarang*);
+	void cetakpengarang();
+};
+
+#endif 
--- exercisedua_157/exercisedua_157.cpp
+++ exercisedua_157/exercisedua_157.cpp
@@ -1,78 +1,78 @@
-#include <iostream>
-#include <vector>
-using namespace std;
+#include <iostream>																				// membuat wadah
+#include <vector>																				// menambahkan wadah vector
+using namespace std;																			// menggunakan namespace standar
 
-class pengarang;
-class penerbit {
-public:
-	string nama_penerbit;
-	vector<pengarang*> daftar_pengarang;
-	penerbit(string pNama) : nama_penerbit(pNama) {
-		cout << "penerbit \"" << nama_penerbit << "\" \n";
+class pengarang;																				// membuat class pengarang
+class penerbit {																				// membuat class penerbit
+public:																							// akses modifier public
+	string nama_penerbit;																		// tipe data string untuk nama penerbit
+	vector<pengarang*> daftar_pengarang;														// menggunakan vector (sejenis array dinamis)
+	penerbit(string pNama) : nama_penerbit(pNama) {												// constructor. membuat objek penerbit dengan parameter pNama untuk nama penerbit
+		cout << "penerbit \"" << nama_penerbit << "\" \n";										// menampilkan nama penerbit
 	}
 
-	void TambahPengarang(pengarang*);
-	void cetakPengarang();
+	void TambahPengarang(pengarang*);															// metod tambahpengarang dengan parameter pengarang
+	void cetakPengarang();																		// metod cetak pengarang
 };
 
-class pengarang {
-public:
-	string nama_pengarang;
-	vector<penerbit*> daftar_penerbit;
-	pengarang(string pNama) : nama_pengarang(pNama) {
-		cout << "pengarang \"" << nama_pengarang << "\" \n";
+class pengarang {																				// membuat class pengarang
+public:																							// akses modifier public
+	string nama_pengarang;																		// tipe data string untuk nama pengarang
+	vector<penerbit*> daftar_penerbit;															// menggunakan vector (sejenis array dinamis)
+	pengarang(string pNama) : nama_pengarang(pNama) {											// constructor. membuat objek penerbit dengan parameter pNama untuk nama pengarang
+		cout << "pengarang \"" << nama_pengarang << "\" \n";									//  menampilkan nama pengarang
 	}
 
-	void TambahPenerbit(penerbit*);
-	void cetakPenerbit();
+	void TambahPenerbit(penerbit*);																// metod tambahpenerbit dengan parameter pengarang
+	void cetakPenerbit();																		// metod cetak penerbit
 };
 
-void penerbit::TambahPengarang(pengarang* pPengarang) {
-	daftar_pengarang.push_back(pPengarang);
+void penerbit::TambahPengarang(pengarang* pPengarang) {											// objek pengarang dengan parameter pPengarang dalam TambahPengarang yang di sinkronkan dengan penerbit
+	daftar_pengarang.push_back(pPengarang);														// progam supaya data baru yang diinputkan selalu berada di akhir
 }
-void penerbit::cetakPengarang() {
-	cout << "\nDaftar pengarang yang menaungi penerbit \"" << this->nama_penerbit << "\":\n";
-	for (auto& a : daftar_pengarang) {
-		cout << a->nama_pengarang << "\n";
+void penerbit::cetakPengarang() {																// mensinkronkan antara cetakPengarang dan penerbit
+	cout << "\nDaftar pengarang yang menaungi penerbit \"" << this->nama_penerbit << "\":\n";	// menampilkan daftar pengarang, this parameter pointer untuk nama penerbit
+	for (auto& a : daftar_pengarang) {															// perulangan for dengan parameter a = daftar nama pengarang
+		cout << a->nama_pengarang << "\n";														// menampilkan parameter pointer a = nama pengarang
 	}
-	cout << endl;
+	cout << endl;																				// tampilkan
 }
 
-void pengarang::TambahPenerbit(penerbit* pPenerbit) {
-	daftar_penerbit.push_back(pPenerbit);
-	pPenerbit->TambahPengarang(this);
+void pengarang::TambahPenerbit(penerbit* pPenerbit) {											// objek penerbit dengan parameter pPenerbit dalam TambahPenerbit yang di sinkronkan dengan pengarang
+	daftar_penerbit.push_back(pPenerbit);														// progam supaya data baru yang diinputkan selalu berada di akhir
+	pPenerbit->TambahPengarang(this);															// pointer parameter pPenerbit = penerbit merujuk pada tambah pengarang (data yang sedang dipilih)
 
 }
 
-void pengarang::cetakPenerbit() {
-	cout << "Daftar penerbit yang dinaungi pengarang \"" << this->nama_pengarang << "\":\n";
-	for (auto& a : daftar_penerbit) {
+void pengarang::cetakPenerbit() {																// menskinkronkan antara cetakpenerbit dengan pengarang
+	cout << "Daftar penerbit yang diikuti pengarang \"" << this->nama_pengarang << "\":\n";		// menampilkan daftar penerbit yang diikuti dengan memanggil this = nama pengarangnya
+	for (auto& a : daftar_penerbit) {															// pengulangan for untuk parameter a = daftar nama penerbit
 		cout << a->nama_penerbit << "\n";
 	}
-	cout << endl;
+	cout << endl;																				// tampilkan
 }
 
 
 int main() {
-	penerbit* VarPenerbit1 = new penerbit("Gama Press");
-	penerbit* VarPenerbit2 = new penerbit("Intan Pariwara");
-	pengarang* VarPengarang1 = new pengarang("Giga");
-	pengarang* VarPengarang2 = new pengarang("Joko");
-	pengarang* VarPengarang3 = new pengarang("Ansori");
-	pengarang* Varpengarang4 = new pengarang("Lia");
+	penerbit* VarPenerbit1 = new penerbit("Gama Press");										// membuat objek baru "penerbit1" yang bernama Gama Press
+	penerbit* VarPenerbit2 = new penerbit("Intan Pariwara");									// membuat objek baru "penerbit2" yang bernama Intan Pariwara
+	pengarang* VarPengarang1 = new pengarang("Giga");											// membuat objek baru "pengarang1" yang bernama Giga
+	pengarang* VarPengarang2 = new pengarang("Joko");											// membuat objek baru "pengarang2" yang bernama joko								
+	pengarang* VarPengarang3 = new pengarang("Asroni");											// membuat objek baru "pengarang3" yang bernama Asroni
+	pengarang* Varpengarang4 = new pengarang("Lia");											// membuat objek baru "pengarang4" yang bernama Lia
 
-	VarPenerbit1->TambahPengarang(VarPengarang2);
-	VarPenerbit1->TambahPengarang(Varpengarang4);
-	VarPenerbit1->TambahPengarang(VarPengarang1);
-	VarPenerbit2->TambahPengarang(VarPengarang3);
-	VarPenerbit2->TambahPengarang(VarPengarang1);
-	VarPengarang1->TambahPenerbit(VarPenerbit1);
-	VarPengarang1->TambahPenerbit(VarPenerbit2);
+	VarPenerbit1->TambahPengarang(VarPengarang2);												// variabel varpenerbit1 yang menjadi pointer pengarang 2 
+	VarPenerbit1->TambahPengarang(Varpengarang4);												// variabel varpenerbit1 yang menjadi pointer pengarang 4 
+	VarPenerbit1->TambahPengarang(VarPengarang1);												// variabel varpenerbit1 yang menjadi pointer pengarang 1 
+	VarPenerbit2->TambahPengarang(VarPengarang3);												// variabel varpenerbit2 yang menjadi pointer pengarang 3 
+	VarPenerbit2->TambahPengarang(VarPengarang1);												// variabel varpenerbit2 yang menjadi pointer pengarang 1 
+	VarPengarang1->TambahPenerbit(VarPenerbit2);												// variabel varpengarang1 yang menjadi pointer varpenerbit2
+	VarPengarang1->TambahPenerbit(VarPenerbit1);												// variabel varpengarang1 yang menjadi pointer varpenerbit1
 
-	VarPenerbit1->cetakPengarang();
-	VarPenerbit2->cetakPengarang();
-	VarPengarang1->cetakPenerbit();
+	VarPenerbit1->cetakPengarang();																// mencetak/menampilkan penerbit 1
+	VarPenerbit2->cetakPengarang();																// mencetak/menampilkan penerbit 2
+	VarPengarang1->cetakPenerbit();																// mencetak/menampilkan pengarang
 
 
-	return 0;
+	return 0;																			
 }
