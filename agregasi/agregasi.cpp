b49c7eb68cc53afb381d7bedd071ae25be519881
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2023-06-05 14:37:24 +0700

    buku

--- agregasi/agregasi.cpp
+++ agregasi/agregasi.cpp
@@ -6,3 +6,9 @@ using namespace std;														// menggunakan namespace standar
 #include "pengarang.h"
 
 int main() {
+	buku* varBuku1 = new buku("Fisika");
+	buku* varBuku2 = new buku("Algoritma");
+	buku* varBuku3 = new buku("BasisData");
+	buku* varBuku4 = new buku("Dasar Pemograman");
+	buku* varBuku5 = new buku("Matematika");
+	buku* varBuku6 = new buku("Multimedia 1");
