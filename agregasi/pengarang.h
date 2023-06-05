5e8c382000a357854f1dd85b7004b8640f31d3f0
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2023-06-05 14:20:15 +0700

    akses modifier dan member

--- agregasi/pengarang.h
+++ agregasi/pengarang.h
@@ -3,5 +3,15 @@
 #define PENGARANG_H
 
 class pengarang {
+public:
+	string nama_pengarang;
+	vector<buku*> daftar_buku;
+	pengarang(string pNama) :nama_pengarang(pNama) {
+		cout << "pengarang \"" << nama_pengarang << "\" \n";
+	}
+
+	void tambahJudulBuku(buku*);
+	void cetakJudulBuku();
+};
 
 #endif
