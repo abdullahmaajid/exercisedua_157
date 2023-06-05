1255bdd44124e6a1eb685924a9f53dbdafab5dc4
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2023-06-05 13:36:59 +0700

    akses modifier dan member dari penerbit

--- exercisedua_157/exercisedua_157.cpp
+++ exercisedua_157/exercisedua_157.cpp
@@ -4,7 +4,16 @@ using namespace std;
 
 class pengarang;
 class penerbit {
+public:
+	string nama_penerbit;
+	vector<pengarang*> daftar_pengarang;
+	penerbit(string pNama) : nama_penerbit(pNama) {
+		cout << "penerbit \"" << nama_penerbit << "\" \n";
+	}
 
+	void TambahPengarang(pengarang*);
+	void cetakPengarang();
+};
 
 
 
